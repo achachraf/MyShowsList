e1976ff837f8c5c1e08a4e1167ec341dba26f7d9
achraf <achrafachkari@hotmail.fr>
2021-07-04 21:01:11 +0200

    Init APP and Creat/Read/Deletes Shows

--- README.md
+++ README.md
@@ -1,70 +1,38 @@
-# Getting Started with Create React App
+## This is a small App from scratch created for educational purposes
+### 1. Installation
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+first download and install node.js (npm included)
 
-## Available Scripts
+>link: https://nodejs.org/en/download/
 
-In the project directory, you can run:
+To create your own react app, run this command
 
-### `npm start`
+    npx create-react-app <App_Name>
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+where you have to replace '<App_name>' by yours
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+If you want just to clone the reposetory, install Git first and then run
 
-### `npm test`
+    git clone https://github.com/achrafxx/MyShowsList.git
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### 2. Run the app
+In both cases, to run the app just run the following command in the app repository:
 
-### `npm run build`
+    npm start
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+if you're under linux and get watchers error, run this command
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+     echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+### 3. Useful links
 
-### `npm run eject`
+>Bootstrap: https://getbootstrap.com/docs
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+>Bootstrap with react: https://blog.logrocket.com/how-to-use-bootstrap-with-react-a354715d1121/
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+>React course (New): https://www.youtube.com/watch?v=j942wKiXFu8&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+Even if I like more the old one: 
+>React course: https://www.youtube.com/watch?v=yZ0f1Apb5CU&list=PL4cUxeGkcC9i0_2FF-WhtRIfIJ1lXlTZR
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,152 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import {getRandomId} from './utils/generators'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    let initShow = {
+      id: null,
+      title: "",
+      status: "Watching...",
+      rate: 0
+    }
+
+    const [show, setShow] = useState(initShow)
+    const [shows, setShows] = useState([])
+
+    useEffect(()=>{
+      if(localStorage.getItem("shows") === null){
+        localStorage.setItem("shows",JSON.stringify([]))
+      }
+      else{
+        let stringShows = localStorage.getItem("shows");
+        setShows(JSON.parse(stringShows))
+      }
+    },[])
+
+    
+    const handleChange = (e)=>{
+      let value = e.target.value;
+      console.log("we are changing the field: "+e.target.id)
+      setShow({
+        ...show,
+        [e.target.id]: value
+      })
+    }
+
+    const handleSubmit  = (e)=>{
+      e.preventDefault();
+      if(show.title !== ""){
+        
+        let updatedShows = [...shows, {...show,id: getRandomId(10)}]
+
+        // add to localstorage
+        localStorage.setItem("shows", JSON.stringify(updatedShows))
+
+        //add to state
+        setShows(updatedShows)
+
+        //cleanning the actual state
+        setShow(initShow)
+
+        //close Modal
+        document.getElementById("closeModal").click();
+
+      }
+    }
+
+    const handleDelete = (id)=>{
+
+      let updatedShows = shows.filter((show)=>(show.id !== id))
+
+      //remove from localstorage
+      localStorage.setItem("shows", JSON.stringify(updatedShows))
+
+      //remove from state
+      setShows(updatedShows)
+
+    }
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="offset-md-2 col-md-8 text-center">
+                    <h1>Welcome to MyShowsList</h1>
+                    <h4>You want to add a show? </h4>
+                    <button className="btn btn-outline-primary"  data-bs-toggle="modal" data-bs-target="#addShowModal">Click me ;)</button>
+                </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <table className="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>#Id</th>
+                      <th>Title</th>
+                      <th>Status</th>
+                      <th>Rate</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {shows.map((show)=>{
+                      return (
+                        <tr key={show.id}>
+                          <td>{show.id}</td>
+                          <td>{show.title}</td>
+                          <td>{show.status}</td>
+                          <td>{show.rate}</td>
+                          <td>
+                            <button onClick={()=>{handleDelete(show.id)}} className="btn btn-danger">Delete</button>
+                          </td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div
+                class="modal fade"
+                id="addShowModal"
+                tabindex="-1"
+                aria-hidden="true"
+            >
+                <div class="modal-dialog">
+                  <form onSubmit={handleSubmit}>
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">
+                                Modal title
+                            </h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <label htmlFor="#title">Title</label>
+                            <input value={show.title} onChange={handleChange} className="form-control" type="text" id="title"/>
+                            <label className="mt-3" htmlFor="#status">Status</label>
+                            <select value={show.status} onChange={handleChange} className="form-select" name="status" id="status">
+                              <option value="watching">Watching...</option>
+                              <option value="planning">Planning to watching</option>
+                              <option value="finished">Finished</option>
+                            </select>
+                            <label  className="mt-3" htmlFor="#rate">Rate</label>
+                            <input value={show.rate} onChange={handleChange} type="number" className="form-control" id="rate"/>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="closeModal">
+                                Close
+                            </button>
+                            <button type="submit" class="btn btn-primary">
+                                Save changes
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
